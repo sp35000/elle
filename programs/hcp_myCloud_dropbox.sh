0d64b419de90677ae0b22a07956b343ae69e6a0c
sp35000 <sp35000@yahoo.com.br>
2022-08-08 09:50:26 -0300

    fix: adjusted to myCloud

--- programs/hcp_myCloud_dropbox.sh
+++ programs/hcp_myCloud_dropbox.sh
@@ -1,26 +1,22 @@
 #!/bin/sh
 # -------------------------------------------------------------
 # App       : Elle
-# Program   : hcp_ownCloud_dropbox.sh
-# Function  : rsync owncloud with dropbox
+# Program   : hcp_myCloud_dropbox.sh
+# Function  : rsync myCloud with dropbox
 # Site      : https://bitbucket.org/sp35000/
 # Author    : Celso Kikuchi <sp35000@yahoo.com.br>
 # -------------------------------------------------------------
 # 20190306: initial version
+# 20220808: adjusted to myCloud
 # -------------------------------------------------------------
 # initialize variables
-SOURCE="/home/yzmu/ownCloud/"
-TARGET="/home/yzmu/Dropbox/ownCloud/"
+SOURCE="/home/yzmu/myCloud/"
+TARGET="/home/yzmu/Dropbox/myCloud/"
 # -------------------------------------------------------------
 # start
 echo "\n----------------------------------------------------------"
 echo "Script: $0 START"
 date
-echo "rsync " $SOURCE $TARGET
-rsync -Cravzp --no-links --delete-before $SOURCE $TARGET
-echo "-------------------------"
-SOURCE="/home/yzmu/myCloud/"
-TARGET="/home/yzmu/Dropbox/myCloud/"
 echo "rsync " $SOURCE $TARGET
 rsync -Cravzp --no-links --delete-before $SOURCE $TARGET
 echo "Script: $0 END"
--- programs/pc18-nightly.sh
+++ programs/pc18-nightly.sh
@@ -7,9 +7,10 @@
 # Author    : Celso Kikuchi <sp35000@yahoo.com.br>
 # -------------------------------------------------------------
 # 20200515: initial version
-# 20210215: feat: separating in main and report
-# 20220806: feat: adapting to intra
+# 20210215: separating in main and report
+# 20220806: adapting to intra
 # 20220807: adapting to work4love
+# 20220808: adjusted to myCloud
 # -------------------------------------------------------------
 # initialize variables
 # -------------------------------------------------------------
@@ -26,7 +27,7 @@ main() {
   /home/yzmu/bin/hrsync_sig_git.sh
   /home/yzmu/bin/hrsync_w4l_git.sh
   #/home/yzmu/bin/hrsync_owncloud-mycloud_mega.sh
-  /home/yzmu/bin/hcp_ownCloud_dropbox.sh
+  /home/yzmu/bin/hcp_myCloud_dropbox.sh
   #/home/yzmu/bin/hcp_docs_case04.sh
   /home/yzmu/bin/serina-w4l-cloud.sh "$PARM"
   /home/yzmu/bin/ftp_home_w4l.sh "$FTP_HOME_CKROPS"
