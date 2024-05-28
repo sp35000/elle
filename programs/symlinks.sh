e8b12c8f44f12df599835071da9d20e9e1ccee8d
sp35000 <sp35000@yahoo.com.br>
2024-05-28 12:26:47 -0300

    feat: add symlinks.sh

--- programs/symlinks.sh
+++ programs/symlinks.sh
@@ -1,27 +1,50 @@
 #!/bin/bash
 # -------------------------------------------------------------
-# App       : 
-# Program   : 
-# Function  : 
+# App       : Elle
+# Program   : symlinks.sh
+# Function  : create/remove symlinks
 # Site      : https://github.com/sp35000/elle
 # Author    : Celso Kikuchi <sp35000@yahoo.com.br>
 # -------------------------------------------------------------
-version="20yymmdd: initial version"
+version="20240528: initial version"
 # -------------------------------------------------------------
 # initialize variables
-msg_help="help"
+msg_help="create/remove symlinks"
+inputfile=$2
 # -------------------------------------------------------------
+# functions
+create_symlinks() {
+    echo "create_symlinks from $inputfile"
+    cat $inputfile | while read linha
+    do
+        symlink=$(echo "$linha"|awk -F '=' '{print $1}')
+        folder=$(echo "$linha"|awk -F '=' '{print $2}')
+        command="ln -s $folder $symlink"
+        $command
+    done     
+}
+
+remove_symlinks() {
+    echo "remove_symlinks from $inputfile"
+    cut -d'=' -f1 $inputfile | while read linha
+    do
+        command="rm $linha"
+        $command
+    done 
+}
 # verify options and define flags
-while getopts "hV" option 
+while getopts "chrV" option 
 do
  case $option in
+  c) create_symlinks; exit 0 ;;
   h) echo "$msg_help"; exit 0 ;;
+  r) remove_symlinks; exit 0 ;;
   V) echo "$version"; exit 0 ;;
   *) exit 1;;
  esac
 done
 
-# functions
+# main program
 main() {
  echo "----------------------------------------------------------"
  echo "Script: $0 START"
