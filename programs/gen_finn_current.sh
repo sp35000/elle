4796e1d11967e699c9c80364fd2446bdb7167f91
sp35000 <sp35000@yahoo.com.br>
2024-08-02 09:40:10 -0300

    feat: updating gen_finn_current.sh

--- programs/gen_finn_current.sh
+++ programs/gen_finn_current.sh
@@ -2,16 +2,17 @@
 # -------------------------------------------------------------
 # App       : Finn
 # Program   : gen_finn_current.sh
-# Function  : generate reduced images for project/finn/current
+# Function  : generate thumbnails
 # Site      : https://github.com/sp35000/elle
 # Author    : Celso Kikuchi <sp35000@yahoo.com.br>
 # -------------------------------------------------------------
 # 20230131: initial version
 # 20230208: adding getty folder
+# 20240801: adjusting for better performance
 # -------------------------------------------------------------
 # initialize variables
-msg_help="help"
-version="0.0"
+msg_help="generate thumbnails"
+version="20240801"
 size="10%"
 sourcedir="/home/yzmu/case04/mirror/photo/project/finn/current"
 targetdir="/home/yzmu/case04/mirror/photo/project/finn/thumb"
@@ -30,8 +31,18 @@ proccessFolder() {
  folder=$1
  cd $sourcedir/$folder
  for file in $(ls *.jpg); do
+# generate thumbnails just if it still was not created
+ if test -e "$targetdir/$folder/$file" 
+ then
+  echo "$folder/$file jumped"
+ else  
+  echo "Generating thumbnail for $folder/$file"...
 # convert-im6.q16 [input-option] input-file [output-option] output-file
-  convert -resize "$size" "$sourcedir/$folder/$file" "$targetdir/$folder/$file"
+# convert runs with low priority
+  nice -n 15 convert -resize "$size" "$sourcedir/$folder/$file" "$targetdir/$folder/$file"
+# sleep 5 seconds to avoid multiple processes at the same time
+  sleep 5
+ fi
  done
 }
 
@@ -39,7 +50,6 @@ main() {
  echo "----------------------------------------------------------"
  echo "Script: $0 START"
  date
- rm -Rf $targetdir
  # main
  for folder in $(ls $sourcedir|grep -v jpg); do
  mkdir -p $targetdir/$folder
