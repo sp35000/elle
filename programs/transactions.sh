3f1feafd8b4ca528085f5f49176e1192cd39b3c0
sp35000 <sp35000@yahoo.com.br>
2025-01-20 16:19:35 -0300

    feat: w1 and transactions updated

--- programs/transactions.sh
+++ programs/transactions.sh
@@ -7,11 +7,14 @@
 # Author    : Celso Kikuchi <sp35000@yahoo.com.br>
 # -------------------------------------------------------------
 # 20210329: improved from initial version
-# 20241230: variables source and target added
+# 20241230: case07 variables source and target added
+# 20250120: pc18 variables source and target added
 # -------------------------------------------------------------
 # initialize variables
-source="/media/yzmu/b07c0ea6-d09c-4030-9710-868b995cd3af/d"
-target="/media/yzmu/b07c0ea6-d09c-4030-9710-868b995cd3af/d"
+# source="/media/yzmu/b07c0ea6-d09c-4030-9710-868b995cd3af/d"
+# target="/media/yzmu/b07c0ea6-d09c-4030-9710-868b995cd3af/d"
+source="/home/yzmu/Documents"
+target="/home/yzmu/Documents"
 read -s -p "tender:" tender
 echo
 # -------------------------------------------------------------
